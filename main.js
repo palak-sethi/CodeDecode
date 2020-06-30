58073aca37e06b2c0b505e5fe933f8695d51ea90
Palak <palaksethi2001@gmail.com>
2020-06-30 18:09:57 +0530

    JS added for string manipulation

--- main.js
+++ main.js
@@ -21,3 +21,40 @@ function decode1() {
     document.getElementById("decodedMessage").innerHTML = rev;
 }
 
+function code2() {
+    var str = document.getElementById("codeMessage").value;
+    var s = str.split("");
+    var n = s.length;
+    var coded = "";
+    for(var i=0; i<n; i++)
+    {
+        var c = s.charCodeAt(i);
+        if((c >= 97 && c <= 109) || (c >= 65 && c <= 77))
+        {
+            c = c+14;
+        }
+        else if((c >= 110 && c <= 122) || (c >= 78 && c <= 90))
+        {
+            c = c-14;
+        }
+        else if((c >= 48 && c <= 52))
+        {
+            c = c + 5;
+        }
+        else if((c >= 53 && c <= 57))
+        {
+            c = c - 5;
+        }
+        else
+        {
+            continue;
+        }
+        s[i]=String.fromCharCode(c);
+    }
+    var coded = str.join("");     
+    document.getElementById("codedMessage").innerHTML = coded;
+}
+
+function decode2() {
+
+}
