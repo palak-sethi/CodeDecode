4f34b3c585230d6153a462ffadc8442112ed186e
Palak <palaksethi2001@gmail.com>
2020-07-01 16:15:23 +0530

    Debugged code2 js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function showDecode()  {
 
 function code1() {
     var str = document.getElementById("codeMessage").value;
-    var rev = str.split("").reverse().join("");
+    var rev = str.split("").reverse().join(""); 
     document.getElementById("codedMessage").innerHTML = rev;
 }
 
@@ -23,19 +23,19 @@ function decode1() {
 
 function code2() {
     var str = document.getElementById("codeMessage").value;
+    var n = str.length;
     var s = str.split("");
-    var n = s.length;
     var coded = "";
     for(var i=0; i<n; i++)
     {
-        var c = s.charCodeAt(i);
+        var c = str.charCodeAt(i);
         if((c >= 97 && c <= 109) || (c >= 65 && c <= 77))
         {
-            c = c+14;
+            c = c+13;
         }
         else if((c >= 110 && c <= 122) || (c >= 78 && c <= 90))
         {
-            c = c-14;
+            c = c-13;
         }
         else if((c >= 48 && c <= 52))
         {
@@ -49,12 +49,42 @@ function code2() {
         {
             continue;
         }
-        s[i]=String.fromCharCode(c);
+        s[i] = String.fromCharCode(c);
     }
-    var coded = str.join("");     
+    var coded = s.join("");     
     document.getElementById("codedMessage").innerHTML = coded;
 }
 
 function decode2() {
-
+    var str = document.getElementById("decodeMessage").value;
+    var n = str.length;
+    var s = str.split("");
+    var coded = "";
+    for(var i=0; i<n; i++)
+    {
+        var c = str.charCodeAt(i);
+        if((c >= 97 && c <= 109) || (c >= 65 && c <= 77))
+        {
+            c = c+13;
+        }
+        else if((c >= 110 && c <= 122) || (c >= 78 && c <= 90))
+        {
+            c = c-13;
+        }
+        else if((c >= 48 && c <= 52))
+        {
+            c = c + 5;
+        }
+        else if((c >= 53 && c <= 57))
+        {
+            c = c - 5;
+        }
+        else
+        {
+            continue;
+        }
+        s[i] = String.fromCharCode(c);
+    }
+    var decoded = s.join("");     
+    document.getElementById("decodedMessage").innerHTML = decoded;
 }
